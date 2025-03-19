2d1d5d5fa1c60f34a49076d91a3c216e26a5c7f3
BenjDW <dewitskibenji@live.fr>
2025-03-19 07:49:06 -0700

    replace main with the subject main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,26 +6,24 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 07:41:22 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/19 07:44:33 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/19 07:48:02 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
-int main()
-{
-    int x = 42, y = 24;
-    double d1 = 5.5, d2 = 2.2;
-
-    std::cout << "Avant swap: x = " << x << ", y = " << y << std::endl;
-    swap(x, y);
-    std::cout << "Après swap: x = " << x << ", y = " << y << std::endl;
-
-    std::cout << "min(3, 7) = " << min(3, 7) << std::endl;
-    std::cout << "max(3, 7) = " << max(3, 7) << std::endl;
-
-    std::cout << "min(d1, d2) = " << min(d1, d2) << std::endl;
-    std::cout << "max(d1, d2) = " << max(d1, d2) << std::endl;
-
-    return 0;
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+return 0;
 }
