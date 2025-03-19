8c9328a17351a4c01b02cac2929617452e44e494
BenjDW <dewitskibenji@live.fr>
2025-03-19 08:45:53 -0700

    remove comments

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,17 +6,9 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 07:50:02 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/19 08:18:36 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/19 08:45:15 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// Implement a function template iter that takes 3 parameters and returns nothing.
-// • The first parameter is the address of an array.
-// • The second one is the length of the array.
-// • The third one is a function that will be called on every element of the array.
-
-// Your iter function template must work with any type of array. The third parameter
-// can be an instantiated function template.
 
 #pragma once
 
