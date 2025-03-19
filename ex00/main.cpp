ab6b1d05dbd6ac728a42b20e8a7bee6829b59af7
BenjDW <dewitskibenji@live.fr>
2025-03-19 07:46:45 -0700

    main done , all is functionnal, remove comments

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,26 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 07:41:22 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/19 07:41:23 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/19 07:44:33 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
+
+int main()
+{
+    int x = 42, y = 24;
+    double d1 = 5.5, d2 = 2.2;
+
+    std::cout << "Avant swap: x = " << x << ", y = " << y << std::endl;
+    swap(x, y);
+    std::cout << "Après swap: x = " << x << ", y = " << y << std::endl;
+
+    std::cout << "min(3, 7) = " << min(3, 7) << std::endl;
+    std::cout << "max(3, 7) = " << max(3, 7) << std::endl;
+
+    std::cout << "min(d1, d2) = " << min(d1, d2) << std::endl;
+    std::cout << "max(d1, d2) = " << max(d1, d2) << std::endl;
+
+    return 0;
+}
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,27 +6,15 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 06:54:32 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/19 07:40:45 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/19 07:44:55 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-// template <typename "nom/type">
-
-// Implement the following function templates:
-
-// • swap: Swaps the values of two given arguments. Does not return anything.
-
-// • min: Compares the two values passed in its arguments and returns the smallest
-// one. If the two of them are equal, then it returns the second one.
-
-// • max: Compares the two values passed in its arguments and returns the greatest one.
-// If the two of them are equal, then it returns the second one.
-
-// These functions can be called with any type of argument. The only requirement is
-// that the two arguments must have the same type and must support all the comparison
-// operators.
+#include <iostream>
+#include <ostream>
+#include <iomanip>
 
 template <typename type>
 void swap(type &V1, type &V2)
